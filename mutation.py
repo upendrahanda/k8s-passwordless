90986fc81c2bab784b63e4a04783a5a1d1133393
Upendra H <72946028+upendrahanda@users.noreply.github.com>
2024-10-29 17:32:06 +0530

    Create mutation.py

---
+++ mutation.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# this module will intercept incoming cluster request
+# through admission control to set environment vars to
+# be used by boto3 in your project running in a pod
